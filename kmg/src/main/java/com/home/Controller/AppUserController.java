2c5e8809fdcff28a306ad440ec4094f76ddd925f
Mohamed Goher <gohar.elsaqqa@gmail.com>
2021-03-28 00:11:13 +0200

    Add cross origin to app user controller.

--- kmg/src/main/java/com/home/Controller/AppUserController.java
+++ kmg/src/main/java/com/home/Controller/AppUserController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,7 @@ import com.home.entities.AppUserEntity;
 import com.home.jsonfilter.View;
 import com.home.repositories.AppUserRepository;
 import com.home.util.ReturnedResultModel;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/rest/user")
 public class AppUserController {
